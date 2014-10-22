9c76705af42456346aadd53f060465c41961f07d
Tony Chung <anthonyuchung@gmail.com>
2014-10-22 01:24:19 -0700

    Setup recipe partials with jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resources :reipces, only: [:index]
+  resources :recipes, only: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
