518bf1eb5c10326f039569110f7f9e54ac0d78ef
Tony Chung <anthonyuchung@gmail.com>
2014-10-23 16:27:10 -0700

    Add tests for Rails show recipe

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resources :recipes, only: [:index]
+  resources :recipes, only: [:index, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
